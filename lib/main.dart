c16cde7427e3e91d700108024200abb7e0e39929
Konrad Kubczyk <konrad@kubczyk.com>
2024-01-21 23:07:24 +0100

    chore: update name

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'PayMe',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
